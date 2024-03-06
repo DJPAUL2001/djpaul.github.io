5f8d52d4f994453dfeabc6dad3bccfcef88f38ee
DJ <dhrubajyotipaul3@gmail.com>
2024-03-06 14:00:37 -0600

    added typed animation

---
+++ assets/js/typed_animation.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typed-text', {
+    strings: ["I love to build tools to help people.", "I love to explore data.", "Live Laugh Learn | Code Compile Cry"],
+    typeSpeed: 60,
+    loop: true,
+    showCursor: false,
+    contentType: 'html'
+});
+  
